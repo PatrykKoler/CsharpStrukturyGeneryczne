59f86027a678b71cfa98cb9d8b2ca5dc3b21b5d9
Patryk <koler.patryk13@gmail.com>
2019-12-05 17:57:04 +0100

    UnitTests

--- 1_TypyGeneryczne/1_TypyGeneryczne/kolejkaKolowa.cs
+++ 1_TypyGeneryczne/1_TypyGeneryczne/kolejkaKolowa.cs
@@ -1,64 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2_KolekcjaGeneryczna
+﻿namespace _1_TypyGeneryczne
 {
-    public class kolejkaKolowa
+    public class KolejkaKolowa
     {
         private double[] bufor;
-        private int poczatek;
-        private int koniec;
+        private int pocztekBufora;
+        private int koniecBufora;
 
-        public kolejkaKolowa() :this(pojemnosc: 5)  
+        public KolejkaKolowa() : this(pojemnosc: 5)
         {
-
         }
-        public kolejkaKolowa(int pojemnosc)
+
+        public KolejkaKolowa(int pojemnosc)
         {
             bufor = new double[pojemnosc + 1];
-            poczatek = 0;
-            koniec = 0;
+            pocztekBufora = 0;
+            koniecBufora = 0;
         }
-        public void zapisz(double wartosc)
+
+        public void Zapisz(double wartosc)
         {
-            bufor[koniec] = wartosc;
-            koniec = (koniec + 1) % bufor.Length;
-            if (koniec == poczatek)
-            {
-                poczatek = poczatek + 1 % bufor.Length;
-            }
+            bufor[koniecBufora] = wartosc;
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+
+            if (koniecBufora == pocztekBufora)
+                pocztekBufora = (pocztekBufora + 1) % bufor.Length;
         }
-        public double czytaj()
+
+        public double Czytaj()
         {
-             var wynik = bufor[poczatek];
-             poczatek = poczatek + 1 % bufor.Length;
-             return wynik;
+            var wynik = bufor[pocztekBufora];
+            pocztekBufora = (pocztekBufora + 1) % bufor.Length;
+            return wynik;
         }
-        public int Pojemosc
+
+        public int Pojemnosc
         {
             get
             {
                 return bufor.Length;
-            }  
+            }
         }
+
         public bool JestPusty
         {
             get
             {
-                return koniec == poczatek;
-            }
-        }
-        public bool jestPelny
-        {
-            get
-            {
-                return (koniec + 1) % bufor.Length == poczatek;
+                return koniecBufora == pocztekBufora;
             }
         }
 
-        
+        public bool JestPelny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == pocztekBufora;
+            }
+        }
     }
 }
---
+++ 1_TypyGeneryczne/1_TypyGeneryczneTests/KolejkaKolowaTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using _1_TypyGeneryczne;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _1_TypyGeneryczneTests
+{
+    [TestClass]
+    public class KolejkaKolowaTest
+    {
+        [TestMethod]
+        public void NowaKolejkaJestPusta()
+        {
+            var kolejka = new KolejkaKolowa();
+
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+        [TestMethod]
+        public void KolejkaTrzyElementowaPoTrzechZapisach()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc : 3);
+            kolejka.Zapisz(3.8);
+            kolejka.Zapisz(5.8);
+            kolejka.Zapisz(6.2);
+            Assert.IsTrue(kolejka.JestPelny);
+        }
+        [TestMethod]
+        public void PierwszyWchodziPierwszyWychodzi()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc : 3);
+
+            var wartosc = 4.6;
+            var wartosc2 = 3.7;
+            kolejka.Zapisz(wartosc);
+            kolejka.Zapisz(wartosc2);
+
+            Assert.AreEqual(wartosc, kolejka.Czytaj());
+            Assert.AreEqual(wartosc2, kolejka.Czytaj());
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+        [TestMethod]
+        public void NadpisywanieElementow()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            var wartosc = new[] { 1.2, 2.3, 3, 4, 5.2, 6.7 };
+
+            foreach (var item in wartosc)
+            {
+                kolejka.Zapisz(item); 
+            }
+            
+            Assert.IsTrue(kolejka.JestPelny);
+            Assert.AreEqual(wartosc[3], kolejka.Czytaj());
+            Assert.AreEqual(wartosc[4], kolejka.Czytaj());
+            Assert.AreEqual(wartosc[5], kolejka.Czytaj());
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+    }
+}
